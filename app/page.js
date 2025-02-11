69464cd5940969da533562039b413d0aad6328b4
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-02-11 11:38:32 +0530

    Header

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
   import {Button} from "@/components/ui/button"
  export default function Home() {
-  return <Button>Welcome to Next.js!</Button>
+  // return <Button>Welcome to Next.js!</Button>
  }
--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,27 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div className= "fixed top-0"> 
+    <div className= "fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b"> 
+    <nav className='container mx-auto flex justify-between items-center py-4 px-4  '>
+      <Link href="/">
+      <Image
+      src={"/logo.png"} alt=' Welth logo' width={200} height={60} 
+      className='h-12 w-auto object-contain'
+      />
+      </Link>
+    
     <SignedOut>
     <SignInButton />
   </SignedOut>
   <SignedIn>
     <UserButton />
-  </SignedIn></div>
+  </SignedIn>
+  </nav>
+  </div>
   )
 }
 
