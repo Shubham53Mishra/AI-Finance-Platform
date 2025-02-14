7c7e2f1935cc0fb7f2baef32755ea162e9718f9e
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-02-15 00:07:40 +0530

    crate the account

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Dashboard = () => {
-  return <div className='px-5'>page</div> 
+  return <div className='px-5'> 
+  {/* Budget progress  */}
+
+  {/* overview */}
+
+  {/* Accounts Grid */}
+  </div> 
         
 }
 
