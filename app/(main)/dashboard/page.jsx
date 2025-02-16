595133c0b50e035370cb9cced769ac216648d184
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-02-16 11:48:08 +0530

    add the card

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,4 +1,5 @@
 import CreateAccountDrawer from '@/components/create-account-drawer'
+import { Card, CardContent } from '@/components/ui/card'
 import React from 'react'
 
 const Dashboard = () => {
@@ -8,12 +9,21 @@ const Dashboard = () => {
   {/* overview */}
 
   {/* Accounts Grid */}
-  <div>
-    <CreateAccountDrawer >
-       open
-    </CreateAccountDrawer>
-  </div>
-  </div> 
+  <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add New Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+        {accounts.length > 0 &&
+          accounts?.map((account) => (
+            <AccountCard key={account.id} account={account} />
+          ))}
+      </div>
+    </div>
         
 }
 
