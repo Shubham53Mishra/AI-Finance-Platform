2f2084b0a65b4762f3dc508945dea0a7603857fd
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-02-16 11:16:13 +0530

    change the route path

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -4,9 +4,22 @@
 
  const CreateAccountDrawer = () => {
    return (
-     <div>
-       CreateAccountDrawer
-     </div>
+    <Drawer>
+  <DrawerTrigger>Open</DrawerTrigger>
+  <DrawerContent>
+    <DrawerHeader>
+      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+      <DrawerDescription>This action cannot be undone.</DrawerDescription>
+    </DrawerHeader>
+    <DrawerFooter>
+      <Button>Submit</Button>
+      <DrawerClose>
+        <Button variant="outline">Cancel</Button>
+      </DrawerClose>
+    </DrawerFooter>
+  </DrawerContent>
+</Drawer>
+
    )
  }
  
